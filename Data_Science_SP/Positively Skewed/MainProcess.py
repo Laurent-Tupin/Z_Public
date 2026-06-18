2f7d050801e4f08ef66c4f3da2f32fb9b74a621d
laurent.tupin <64402274+Laurent-Tupin@users.noreply.github.com>
2026-06-18 15:27:35 +0800

    Create MainProcess.py

---
+++ Data_Science_SP/Positively Skewed/MainProcess.py
@@ -0,0 +1,265 @@
+#from learntools.core import binder
+#binder.bind(globals())
+
+import pandas as pd
+import numpy as np
+import os
+from collections import OrderedDict
+from sklearn.base import clone
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import make_scorer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, median_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBRegressor
+
+
+#------------------------------------------------------------------------------
+# Score Model
+#------------------------------------------------------------------------------
+def mape(y_true, y_pred):
+    y_val = np.maximum(np.array(y_true), 1e-8)
+    MAE = np.abs(y_true - y_pred).mean()
+    print('   MAE', MAE)
+    mape = (np.abs(y_true - y_pred)/y_val).mean()
+    print('   mape', mape)
+    return mape
+
+def fMAE_scoreModel(o_model, X_t, X_v, y_t, y_v):
+    o_model.fit(X_t, y_t)
+    y_pred = o_model.predict(X_v)
+    #mape(y_v, y_pred)
+    MAE = mean_absolute_error(y_v, y_pred)
+    return MAE
+
+def fMAE_scoreModel_Split(o_model, X, y):
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size = 0.8, test_size = 0.2, random_state = 0)
+    o_model.fit(X_train, y_train)
+    y_pred = o_model.predict(X_valid)
+    MAE = mean_absolute_error(y_valid, y_pred)
+    return MAE
+
+def fXMAE_scoreModel_pipeline(o_Pipeline, X, y, int_cv = 5, str_scoring = 'neg_mean_absolute_error'):
+    l_MAE = -1 * cross_val_score(o_Pipeline, X, y, cv = int_cv, scoring = str_scoring)
+    return l_MAE.mean()
+    
+def fXMAE_scoreModel(o_model, X, y, int_cv = 5, str_scoring = 'neg_mean_absolute_error'):
+    # Filter Num and Categ Columns
+    numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns.to_list()
+    categorical_cols = X.select_dtypes(include=['object']).columns.to_list()
+    # Fonction de transformation
+    numerical_transformer = SimpleImputer(strategy = 'median')
+    categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy = 'constant')),            #'most_frequent'
+                                              ('onehot', OneHotEncoder(handle_unknown = 'ignore'))])
+    # Preprocess Data
+    preprocessor = ColumnTransformer(transformers=[('num', numerical_transformer, numerical_cols),
+                                                   ('cat', categorical_transformer, categorical_cols)])
+    # Pipeline Data And Model
+    my_pipeline = Pipeline(steps = [('preprocessor', preprocessor),('model', o_model)])
+    XMAE = fXMAE_scoreModel_pipeline(my_pipeline, X, y, int_cv, str_scoring)
+    return XMAE
+    
+    
+    
+    
+    
+    
+
+
+#------------------------------------------------------------------------------
+# CLEAN DATA
+#------------------------------------------------------------------------------
+def fBl_IsColEmpty(df, flt_pct = 0.5):
+    # Check the number of NA per column first (if more than 50%... maybe delete the column will be better)
+    num_rows = df.shape[0]
+    X_nbNA = (df.isnull().sum())
+    l_nbNA = [i_col for i_col in X_nbNA if i_col > num_rows * flt_pct]
+    if l_nbNA:
+        print('-------------------------------------------')
+        print('df has {} nb of rows'.format(num_rows))
+        print(' - Missing value per column: ')
+        print(X_nbNA[X_nbNA > 0])
+        print(' - Consider deleting the column')
+        print('\n')
+        
+        
+#------------------------------------------------------------------------------
+# Chose the best way to clean DATA
+#------------------------------------------------------------------------------ 
+def f_CompareDataMethod(o_model, X_t, X_v, y_train, y_valid):
+    d_choice = {}
+    l_cols_wNA = [col for col in X_t.columns if X_t[col].isnull().any() or X_v[col].isnull().any()]
+    if not l_cols_wNA:
+        print("  * No column with missing values")
+        return 0, d_choice
+    int_MAE_dropCol = fMAE_dropCol(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid)
+    int_MAE_ImputerMean = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'mean')
+    int_MAE_ImputerMean_keepM = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'mean', True)
+    int_MAE_ImputerMedian = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'median')
+    int_MAE_ImputerMedian_keepM = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'median', True)
+    int_MAE_ImputerConst = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'constant')
+    #    int_MAE_ImputerConst_keepM = fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, 'constant', True)
+    
+    d_choice[int_MAE_dropCol] = [0, 'Drop columns']
+    d_choice[int_MAE_ImputerMean] = [1, 'Imputer Mean']
+    d_choice[int_MAE_ImputerMean_keepM] = [2, 'Imputer Mean Keep missing']
+    d_choice[int_MAE_ImputerMedian] = [3, 'Imputer median']
+    d_choice[int_MAE_ImputerMedian_keepM] = [4, 'Imputer Median Keep missing']
+    d_choice[int_MAE_ImputerConst] = [5, 'Imputer constant']
+    #    d_choice[int_MAE_ImputerConst_keepM] = [6, 'Imputer constant Keep missing']
+    
+    int_MAE = min([int_MAE_dropCol, int_MAE_ImputerMean, int_MAE_ImputerMedian, 
+                   int_MAE_ImputerMean_keepM, int_MAE_ImputerMedian_keepM,
+                   int_MAE_ImputerConst])
+    return int_MAE, d_choice
+
+def fMAE_dropCol(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid):
+    X_t_reduced = X_t.drop(l_cols_wNA, axis=1)
+    X_v_reduced = X_v.drop(l_cols_wNA, axis=1)
+    if len(X_t_reduced.columns) > 0:
+        MAE = fMAE_scoreModel(o_model, X_t_reduced, X_v_reduced, y_train, y_valid)
+    else:
+        print('  fMAE_dropCol return no Columns. Drop This idea')
+        return 1000000000
+    return MAE
+
+def fMAE_Imputer(o_model, l_cols_wNA, X_t, X_v, y_train, y_valid, str_strategy = 'mean', bl_keepMissingCol = False):
+    if bl_keepMissingCol:
+        X_t_imputed, X_v_imputed = fDf_replaceNAImputer_keepMissing(X_t, X_v, l_cols_wNA, str_strategy)
+    else:
+        X_t_imputed, X_v_imputed = fDf_replaceNAImputer(X_t, X_v, str_strategy)
+    MAE = fMAE_scoreModel(o_model, X_t_imputed, X_v_imputed, y_train, y_valid)
+    return MAE
+
+def fDf_replaceNAImputer(X_train, X_valid, str_strategy = 'mean'):
+    # Replace missing values with the mean value along each column
+    my_imputer = SimpleImputer(strategy = str_strategy)
+    imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+    imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+    # Imputation removed column names; put them back
+    imputed_X_train.columns = X_train.columns
+    imputed_X_valid.columns = X_valid.columns
+    return imputed_X_train, imputed_X_valid
+
+def fDf_replaceNAImputer_keepMissing(X_train, X_valid, l_cols_wNA, str_strategy = 'mean'):
+    X_train_plus = X_train.copy()
+    X_valid_plus = X_valid.copy()
+    for col in l_cols_wNA:
+        X_train_plus[col + '_was_missing'] = X_train_plus[col].isnull()
+        X_valid_plus[col + '_was_missing'] = X_valid_plus[col].isnull()
+    X_train_plus, X_valid_plus = fDf_replaceNAImputer(X_train_plus, X_valid_plus, str_strategy)
+    return X_train_plus, X_valid_plus
+
+
+#------------------------------------------------------------------------------
+# Reform Data
+#------------------------------------------------------------------------------   
+def f_RefundData(X_t, X_v, i_choice = 4):
+    l_cols_wNA = [col for col in X_t.columns if X_t[col].isnull().any() or X_v[col].isnull().any()]
+    if i_choice == 0:
+        print('You need to write the code if delete column need to be deleted. also for X_Res and y_res')
+        return X_t, X_v
+    elif i_choice == 1:
+        X_train, X_valid = fDf_replaceNAImputer(X_t, X_v, 'mean')
+    elif i_choice == 2:
+        X_train, X_valid = fDf_replaceNAImputer_keepMissing(X_t, X_v, l_cols_wNA, 'mean')
+    elif i_choice == 3:
+        X_train, X_valid = fDf_replaceNAImputer(X_t, X_v, 'median')
+    elif i_choice == 4:
+        X_train, X_valid = fDf_replaceNAImputer_keepMissing(X_t, X_v, l_cols_wNA, 'median')
+    elif i_choice == 5:
+        X_train, X_valid = fDf_replaceNAImputer(X_t, X_v, 'constant')
+    elif i_choice == 6:
+        X_train, X_valid = fDf_replaceNAImputer_keepMissing(X_t, X_v, l_cols_wNA, 'constant')
+    else:
+        print('   ERROR In f_RefundData: NEED to fill another i_choice: {}'.format(str(i_choice)))
+        return X_t, X_v
+    return X_train, X_valid
+    
+
+#------------------------------------------------------------------------------
+# Test Model - Get the best parameters with 'RandomForestRegressor' model
+#------------------------------------------------------------------------------
+def f_findModelParameters(X_train, X_valid, y_train, y_valid):
+    i_samplesSplit = 2
+#    i_samplesSplit = fInt_Get_min_samples_split(X_train, X_valid, y_train, y_valid, [2,10,30,50,70])
+#    print('   i_samplesSplit: ', i_samplesSplit)
+    i_estim = 2000
+#    i_estim = fInt_Get_n_estimators(X_train, X_valid, y_train, y_valid, [10,50,100,200,500,1000,2000,5000], i_samplesSplit)
+#    print('   i_estim: ', i_estim)
+    i_maxLeafNodes = 5000
+#    i_maxLeafNodes = fInt_Get_max_leaf_nodes(X_train, X_valid, y_train, y_valid, [None,5,100,500,5000,10000], i_samplesSplit, i_estim)
+#    print('   i_maxLeafNodes: ', i_maxLeafNodes)
+    i_maxDepth = None
+#    i_maxDepth = fInt_Get_max_depth(X_train, X_valid, y_train, y_valid, [None,1,20,50], i_samplesSplit, i_estim, i_maxLeafNodes)
+#    print('   i_maxDepth: ', i_maxDepth)
+    str_criterion = 'mse'
+#    str_criterion = fInt_Get_criterion(X_train, X_valid, y_train, y_valid, ['mse','mae'], i_samplesSplit, i_estim, i_maxLeafNodes, i_maxDepth)
+#    print('   str_criterion: ', str_criterion)
+    model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit, n_estimators = i_estim, 
+                                  max_leaf_nodes = i_maxLeafNodes, max_depth = i_maxDepth, criterion = str_criterion)
+    return model
+
+def fInt_Get_min_samples_split(X_train, X_valid, y_train, y_valid, l_toTest):
+    df_MAE = pd.DataFrame(columns = ['MAE', 'XMAE', 'min_samples_split'])
+    for i_samplesSplit in l_toTest:
+        model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit)
+        MAE = fMAE_scoreModel(model, X_train, X_valid, y_train, y_valid)
+        XMAE = fXMAE_scoreModel(model, pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]), 10)
+        df_MAE.loc[len(df_MAE)] = [MAE, XMAE, i_samplesSplit]
+    df_MAE.sort_values(by = ['XMAE'], ascending = True, inplace = True)
+    try:        return int(df_MAE['min_samples_split'].values[0])
+    except:     return None
+
+def fInt_Get_n_estimators(X_train, X_valid, y_train, y_valid, l_toTest, i_samplesSplit):
+    df_MAE = pd.DataFrame(columns = ['MAE', 'XMAE', 'min_samples_split', 'n_estimators'])
+    for i_estim in l_toTest:
+        model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit, n_estimators = int(i_estim))
+        MAE = fMAE_scoreModel(model, X_train, X_valid, y_train, y_valid)
+        XMAE = fXMAE_scoreModel(model, pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]), 10)
+        df_MAE.loc[len(df_MAE)] = [MAE, XMAE, i_samplesSplit, i_estim]
+    df_MAE.sort_values(by = ['XMAE'], ascending = True, inplace = True)
+    try:        return int(df_MAE['n_estimators'].values[0])
+    except:     return None
+
+def fInt_Get_max_leaf_nodes(X_train, X_valid, y_train, y_valid, l_toTest, i_samplesSplit, i_estim):
+    df_MAE = pd.DataFrame(columns = ['MAE', 'XMAE', 'min_samples_split', 'n_estimators', 'max_leaf_nodes'])
+    for i_leafNodes in l_toTest:
+        model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit, n_estimators = i_estim, max_leaf_nodes = i_leafNodes)
+        MAE = fMAE_scoreModel(model, X_train, X_valid, y_train, y_valid)
+        XMAE = fXMAE_scoreModel(model, pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]), 10)
+        df_MAE.loc[len(df_MAE)] = [MAE, XMAE, i_samplesSplit, i_estim, i_leafNodes]
+    df_MAE.sort_values(by = ['XMAE'], ascending = True, inplace = True)
+    try:        return int(df_MAE['max_leaf_nodes'].values[0])
+    except:     return None
+    
+def fInt_Get_max_depth(X_train, X_valid, y_train, y_valid, l_toTest, i_samplesSplit, i_estim, i_leafNodes):
+    df_MAE = pd.DataFrame(columns = ['MAE', 'XMAE', 'min_samples_split', 'n_estimators', 'max_leaf_nodes', 'max_depth'])
+    for i_maxDepth in l_toTest:
+        model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit, n_estimators = i_estim, 
+                                      max_leaf_nodes = i_leafNodes, max_depth = i_maxDepth)
+        MAE = fMAE_scoreModel(model, X_train, X_valid, y_train, y_valid)
+        XMAE = fXMAE_scoreModel(model, pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]), 10)
+        df_MAE.loc[len(df_MAE)] = [MAE, XMAE, i_samplesSplit, i_estim, i_leafNodes, i_maxDepth]
+    df_MAE.sort_values(by = ['XMAE'], ascending = True, inplace = True)
+    try:        return int(df_MAE['max_depth'].values[0])
+    except:     return None
+    
+def fInt_Get_criterion(X_train, X_valid, y_train, y_valid, l_toTest, i_samplesSplit, i_estim, i_leafNodes, i_maxDepth):
+    df_MAE = pd.DataFrame(columns = ['MAE', 'XMAE', 'min_samples_split', 'n_estimators', 'max_leaf_nodes', 'max_depth', 'criterion'])
+    for str_criterion in l_toTest:
+        model = RandomForestRegressor(random_state = 0,min_samples_split = i_samplesSplit, n_estimators = i_estim, 
+                                      max_leaf_nodes = i_leafNodes, max_depth = i_maxDepth, criterion = str_criterion)
+        MAE = fMAE_scoreModel(model, X_train, X_valid, y_train, y_valid)
+        XMAE = fXMAE_scoreModel(model, pd.concat([X_train, X_valid]), pd.concat([y_train, y_valid]), 10)
+        df_MAE.loc[len(df_MAE)] = [MAE, XMAE, i_samplesSplit, i_estim, i_leafNodes, i_maxDepth, str_criterion]
+    df_MAE.sort_values(by = ['XMAE'], ascending = True, inplace = True)
+    try:        return str(df_MAE['str_criterion'].values[0])
+    except:     return None
+
+
+
